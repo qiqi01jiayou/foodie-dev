b23a4a4a6016bf61e8ada11710d6be1c782fd90e
qiqi01jiayou <371326319@qq.com>
2020-05-22 00:34:22 +0800

    522

--- foodie-dev-api/src/main/java/com/cjq/Controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/cjq/Controller/IndexController.java
@@ -2,7 +2,11 @@ package com.cjq.Controller;
 
 import com.cjq.enums.YesOrNo;
 import com.cjq.pojo.Carousel;
+import com.cjq.pojo.Category;
+import com.cjq.pojo.vo.CategoryVO;
+import com.cjq.pojo.vo.NewItemsVO;
 import com.cjq.service.CarouselService;
+import com.cjq.service.CategoryService;
 import com.cjq.utils.JSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,8 +29,8 @@ public class IndexController {
     @Autowired
     private CarouselService carouselService;
 
-   /* @Autowired
-    private CategoryService categoryService;*/
+    @Autowired
+    private CategoryService categoryService;
 
     @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
     @GetMapping("/carousel")
@@ -35,44 +39,44 @@ public class IndexController {
         return JSONResult.ok(list);
     }
 
-   /* *//**
+    /* *
      * 首页分类展示需求：
      * 1. 第一次刷新主页查询大分类，渲染展示到首页
      * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
-     *//*
+     */
     @ApiOperation(value = "获取商品分类(一级分类)", notes = "获取商品分类(一级分类)", httpMethod = "GET")
     @GetMapping("/cats")
-    public IMOOCJSONResult cats() {
+    public JSONResult cats() {
         List<Category> list = categoryService.queryAllRootLevelCat();
-        return IMOOCJSONResult.ok(list);
-    }*/
+        return JSONResult.ok(list);
+    }
 
-    /*@ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
+    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
     @GetMapping("/subCat/{rootCatId}")
-    public IMOOCJSONResult subCat(
+    public JSONResult subCat(
             @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
             @PathVariable Integer rootCatId) {
 
         if (rootCatId == null) {
-            return IMOOCJSONResult.errorMsg("分类不存在");
+            return JSONResult.errorMsg("分类不存在");
         }
 
         List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
-        return IMOOCJSONResult.ok(list);
-    }*/
+        return JSONResult.ok(list);
+    }
 
-    /*@ApiOperation(value = "查询每个一级分类下的最新6条商品数据", notes = "查询每个一级分类下的最新6条商品数据", httpMethod = "GET")
+    @ApiOperation(value = "查询每个一级分类下的最新6条商品数据", notes = "查询每个一级分类下的最新6条商品数据", httpMethod = "GET")
     @GetMapping("/sixNewItems/{rootCatId}")
-    public IMOOCJSONResult sixNewItems(
+    public JSONResult sixNewItems(
             @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
             @PathVariable Integer rootCatId) {
 
         if (rootCatId == null) {
-            return IMOOCJSONResult.errorMsg("分类不存在");
+            return JSONResult.errorMsg("分类不存在");
         }
 
         List<NewItemsVO> list = categoryService.getSixNewItemsLazy(rootCatId);
-        return IMOOCJSONResult.ok(list);
-    }*/
+        return JSONResult.ok(list);
+    }
 
 }
---
+++ foodie-dev-pojo/src/main/java/com/cjq/pojo/vo/SubCategoryVO.java
@@ -0,0 +1,13 @@
+package com.cjq.pojo.vo;
+
+import lombok.Data;
+
+@Data
+public class SubCategoryVO {
+
+    private Integer subId;
+    private String subName;
+    private String subType;
+    private Integer subFatherId;
+
+}
